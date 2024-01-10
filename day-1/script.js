21e9968a49480b33ff158e5765e6aaf308744d19
Rebecca <108008511+Rclarkeweb@users.noreply.github.com>
2024-01-10 15:59:24 +0000

    push files

--- day-1/script.js
+++ day-1/script.js
@@ -1,17 +1,16 @@
 const play = (e) => {
-  alert(e);
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
 
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
- };
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+};
 
 const removetransition = (e) => {
   if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
+  e.target.classList.remove('playing');
 };
 
 const keys = document.querySelectorAll('.key');
